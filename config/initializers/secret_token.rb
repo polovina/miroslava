8b1e5d31bf89ded481348ebc07b230def1964cd8
polovina <alex.polovinko@gmail.com>
2011-10-09 23:02:38 -0700

    miroslava

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miroslava::Application.config.secret_token = 'e043435056f094093e72e49500b93e9777fddb66ba3c9ca96187f84f7044f2d6352e12cf3a9f2b72001702593c92b9dbaca05bb5a63040df207b3cd8f5cc1682'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Miroslava::Application.config.session_store :cookie_store, :key => '_miroslava_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Miroslava::Application.config.session_store :active_record_store
